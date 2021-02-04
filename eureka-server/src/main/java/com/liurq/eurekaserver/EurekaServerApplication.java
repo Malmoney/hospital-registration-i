b15c4ecd34f6979cbb200048c01420f9116e482d
buyi <buyi0509@163.com>
2021-02-04 16:57:55 +0800

    sdfs

---
+++ eureka-server/src/main/java/com/liurq/eurekaserver/EurekaServerApplication.java
@@ -0,0 +1,18 @@
+package com.liurq.eurekaserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+/**
+ * @Author:hyz
+ * @Date:2021-01-28
+ * @Desc:
+ **/
+@SpringBootApplication
+@EnableEurekaServer
+public class EurekaServerApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(EurekaServerApplication.class);
+    }
+}
