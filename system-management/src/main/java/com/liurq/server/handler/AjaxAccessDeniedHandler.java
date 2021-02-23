316b513c333cd09ed1be68b2794fd6724420cb7d
Malmoney <1830545274@qq.com>
2021-02-23 10:13:54 +0800

    token登录验证登录已完成100% 权限验证完成35%

--- system-management/src/main/java/com/liurq/server/handler/AjaxAccessDeniedHandler.java
+++ system-management/src/main/java/com/liurq/server/handler/AjaxAccessDeniedHandler.java
@@ -1,14 +1,17 @@
 package com.liurq.server.handler;
 
 import com.liurq.server.restful.rsp.RspInfo;
+import com.liurq.util.JsonUtils;
 import org.springframework.security.access.AccessDeniedException;
 import org.springframework.security.web.access.AccessDeniedHandler;
 import org.springframework.stereotype.Component;
 
 import javax.servlet.ServletException;
+import javax.servlet.ServletOutputStream;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
+import java.nio.charset.StandardCharsets;
 
 /**
  * @Author:hyz
@@ -20,7 +23,13 @@ public class AjaxAccessDeniedHandler implements AccessDeniedHandler {
 
     @Override
     public void handle(HttpServletRequest httpServletRequest, HttpServletResponse httpServletResponse, AccessDeniedException e) throws IOException, ServletException {
-        httpServletResponse.sendError(-999,"权限不足");
-        //httpServletResponse.getWriter().write(RspInfo.success("300","权限不足").toString());
+        httpServletResponse.setHeader("Content-type", "text/json;charset=UTF-8");
+        httpServletResponse.setStatus(301);
+
+        RspInfo<String> rspInfo = new RspInfo<>();
+        rspInfo.setRspCode("-999");
+        rspInfo.setRspDesc("权限不足");
+
+        httpServletResponse.getWriter().write(JsonUtils.objectToJson(rspInfo));
     }
 }
--- system-management/src/main/java/com/liurq/server/handler/AjaxAuthenticationEntryPoint.java
+++ system-management/src/main/java/com/liurq/server/handler/AjaxAuthenticationEntryPoint.java
@@ -1,6 +1,7 @@
 package com.liurq.server.handler;
 
 import com.liurq.server.restful.rsp.RspInfo;
+import com.liurq.util.JsonUtils;
 import org.springframework.security.core.AuthenticationException;
 import org.springframework.security.web.AuthenticationEntryPoint;
 import org.springframework.stereotype.Component;
@@ -9,6 +10,7 @@ import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
+import java.nio.charset.StandardCharsets;
 
 /**
  * @Author:hyz
@@ -19,7 +21,13 @@ import java.io.IOException;
 public class AjaxAuthenticationEntryPoint implements AuthenticationEntryPoint {
     @Override
     public void commence(HttpServletRequest httpServletRequest, HttpServletResponse httpServletResponse, AuthenticationException e) throws IOException, ServletException {
-        httpServletResponse.sendError(-888,"未登录");
-        //httpServletResponse.getWriter().write(RspInfo.success("301","未登录").toString());
+        httpServletResponse.setHeader("Content-type", "text/json;charset=UTF-8");
+        httpServletResponse.setStatus(301);
+
+        RspInfo<String> rspInfo = new RspInfo<>();
+        rspInfo.setRspCode("-888");
+        rspInfo.setRspDesc("未登录");
+
+        httpServletResponse.getWriter().write(JsonUtils.objectToJson(rspInfo));
     }
 }
