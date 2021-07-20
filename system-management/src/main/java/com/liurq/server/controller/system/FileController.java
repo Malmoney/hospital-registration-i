1ed84fb10cd4bee5da0dd4573160784b09cb3fa6
Malmoney <1830545274@qq.com>
2021-07-20 19:24:23 +0800

    阿里云漏洞修复

--- system-management/src/main/java/com/liurq/server/controller/system/FileController.java
+++ system-management/src/main/java/com/liurq/server/controller/system/FileController.java
@@ -43,8 +43,8 @@ public class FileController {
         // yourEndpoint填写Bucket所在地域对应的Endpoint。以华东1（杭州）为例，Endpoint填写为https://oss-cn-hangzhou.aliyuncs.com。
         String endpoint = "https://oss-cn-beijing.aliyuncs.com";
         // 阿里云账号AccessKey拥有所有API的访问权限，风险很高。强烈建议您创建并使用RAM用户进行API访问或日常运维，请登录RAM控制台创建RAM用户。
-        String accessKeyId = "LTAI5tCaviD9jE8NkYMUgk4y";
-        String accessKeySecret = "axakmYu0PZfcoR2YtLFxeVHuO35S5Y";
+        String accessKeyId = "123";
+        String accessKeySecret = "123";
 
         // 创建OSSClient实例。
         OSS ossClient = new OSSClientBuilder().build(endpoint, accessKeyId, accessKeySecret);
